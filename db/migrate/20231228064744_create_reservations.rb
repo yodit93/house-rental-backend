9161084d5551703bfec6adb473e2739ed2970ccf
yodit93 <yoditabebe9391@gmail.com>
2023-12-28 09:55:23 +0300

    Generate migrations for reservation

---
+++ db/migrate/20231228064744_create_reservations.rb
@@ -0,0 +1,15 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.integer :id
+      t.string :status
+      t.date :start_date
+      t.date :end_date
+      t.integer :total_price
+      t.references :user, null: false, foreign_key: true
+      t.references :house, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
